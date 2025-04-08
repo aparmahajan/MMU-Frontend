a08297770138ad767572004b57e33fb7c300d940
aparmahajan <aparmahajan7@g.ucla.edu>
2025-04-07 19:50:02 -0700

    Merge branch 'apar'

--- src/pages/connections.jsx
+++ src/pages/connections.jsx
@@ -1,16 +1,15 @@
 import { useNavigate } from "react-router-dom";
-import { Link } from "react-router-dom";
 
-function connections(){
-
+function Connections() {
   const navigate = useNavigate();
 
   return (
     <div>
       <h1>My connections:</h1>
-      <button onClick={() => navigate("/login")}>Go to Profile</button>
+      <button onClick={() => navigate("/")}>Go to Profile</button>
     </div>
   );
 }
 
-export default connections;
+export default Connections;
+
