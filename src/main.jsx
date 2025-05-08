f38cb5cdc03e589b7e125c3d53aa7202fac7bcd2
aparmahajan <aparmahajan7@g.ucla.edu>
2025-05-08 12:31:39 -0700

    separate url for test and deployment

--- src/main.jsx
+++ src/main.jsx
@@ -22,13 +22,22 @@ import './styles/index.css'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AuthProvider } from "react-oidc-context";
 
+const redirectUrl = import.meta.env.PROD
+  ? import.meta.env.VITE_REDIRECT_URL_PROD
+  : import.meta.env.VITE_REDIRECT_URL;
+
+console.log("VITE_REDIRECT_URL:", import.meta.env.VITE_REDIRECT_URL);
+console.log("VITE_REDIRECT_URL_PROD:", import.meta.env.VITE_REDIRECT_URL_PROD);
+
 const cognitoAuthConfig = {
   authority: "https://cognito-idp.us-west-1.amazonaws.com/us-west-1_z5hk8UrYU",
   client_id: "1ckqdl8f23jn6ms1d622nna6r9",
-  redirect_uri: "http://localhost:5173",
+  redirect_uri: redirectUrl,
   response_type: "code",
   scope: "email openid phone",
 };
+
+console.log("Redirect URL:", redirectUrl); 
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 
